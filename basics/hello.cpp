b7f43caeaee84995faa0410a4c2d26795902f1b8
Chetan Chittori <chetanchittori@gmail.com>
2026-01-07 20:00:20 +0530

    some changes

---
+++ basics/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"c++ practice repo ready"<<endl;
+    return 0;
+}
---
+++ cp/template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        // solve here
+    }
+    return 0;
+}
