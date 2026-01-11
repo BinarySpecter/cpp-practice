dcb7c92e34794f727938605076c73a643b1c733f
Chetan Chittori <chetanchittori@gmail.com>
2026-01-11 15:48:13 +0530

    headerfile

---
+++ basics/headerfile.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h> //instead of including all the header files one by one you can use it.
+ using namespace std;
+
+ int main(){
+    vector<int> v;
+    double x=2.5;
+    cout<<ceil(x);
+ }
